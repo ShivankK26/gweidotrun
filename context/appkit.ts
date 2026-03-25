8a7066e72fcc1e9eb9f048a9251e1a458dd2cc18
Shivank Kapur <shivankkapur2004@gmail.com>
2026-03-25 16:35:18 +0530

    feat: Update project ID and dynamic URL generation in AppKit configuration. Disable analytics feature for improved performance.

--- context/appkit.ts
+++ context/appkit.ts
@@ -5,7 +5,10 @@ import { EthersAdapter } from "@reown/appkit-adapter-ethers";
 import { mainnet } from "@reown/appkit/networks";
 
 const projectId =
-  process.env.NEXT_PUBLIC_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694";
+  process.env.NEXT_PUBLIC_PROJECT_ID || "95f08db78c4d3c32fd283c319fcbe301";
+
+const origin =
+  typeof window !== "undefined" ? window.location.origin : "http://localhost:3000";
 
 createAppKit({
   adapters: [new EthersAdapter()],
@@ -15,11 +18,11 @@ createAppKit({
   metadata: {
     name: "gwei.run",
     description: "gas horse mempool race",
-    url: "https://gwei.run",
+    url: origin,
     icons: ["https://gwei.run/icon.png"],
   },
   features: {
-    analytics: true,
+    analytics: false,
   },
 });
 
